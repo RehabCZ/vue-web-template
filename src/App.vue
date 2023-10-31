28d26120e3306bbca03de7e5fe3011d9e4acea7b
RehabCZ <filipvacek67@gmail.com>
2023-10-31 21:15:13 +0100

    assets

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@ import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
-    <div>
+    <div class="flex justify-center">
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
+        </a>
         <a href="https://vuejs.org/" target="_blank">
             <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
         </a>
