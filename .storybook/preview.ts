36058b9192a61727707b4916b1d497320adc2a65
RehabCZ <71905162+RehabCZ@users.noreply.github.com>
2023-12-15 10:10:48 +0100

    Update preview.ts
    import all stylesheets to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,9 @@
 import type { Preview } from '@storybook/vue3';
 
+// @ts-ignore
+const scss = import.meta.glob("../src/**/*.@(css|scss)");
+Object.keys(scss).forEach((str) => import(/* @vite-ignore */ str));
+
 const preview: Preview = {
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
